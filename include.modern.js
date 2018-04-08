73765bea5ad9fe4cbbdc75742ed69ffc78838afa
johnny_shaman <johnny_shaman@live.jp>
2018-04-08 14:43:18 +0000

    modified:   include.modern.js 	modified:   package.json

--- include.modern.js
+++ include.modern.js
@@ -1,4 +1,7 @@
 (() => {
+    let setSrc = (a, b) => a.setAttribute("src", b) || a;
+    let onLoad = (a, b) => a.addEventListener("load", b, false) || a;
+    let FNToMN = (s) => s.slice(s.length - 3) === ".js" ? s.slice(0, s.length - 3) : s;
     let include = (Object.getPrototypeOf(this.constructor.prototype) !== null && Object.getPrototypeOf(this.constructor.prototype).constructor.name === "WorkerGlobalScope") ?
             importScripts
         : this.constructor.name === "Object" ?
@@ -6,15 +9,15 @@
                 return a.reduceRight((p, c) => Object.assign(p, c.constructor === Object ?
                     Object.keys(c).forEach((k) => c[k] = require(c[k])) || c
                 :
-                    {[c.slice(c.length - 3) === ".js" ? c.slice(0, c.length - 3) : c]: require(c)}
+                    {[FNToMN(c)]: require(c)}
                 ), a[a.length - 1].constructor === String ? this : a.pop());
             }
         :
             function (...a) {
-                let s = document.createElement("script");
-                s.setAttribute("src", a.shift());
-                a.length > 0 && s.addEventListener.addEventListener("load", include(...a));
-                document.body.appendChild(s);
+                let c = a.shift();
+                c !== undefined && this[FNToMN(c)] === undefined ? document.body.appendChild(onLoad(setSrc(
+                    document.createElement("script"), c
+                ), () => include(...a))): include(...a);
             };
 
     this.constructor.name === "Object" && (module.exports = include);
