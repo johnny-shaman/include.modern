02fca16f6dac3fa999eedf527b44dbd6b3e377aa
johnny_shaman <johnny_shaman@live.jp>
2018-04-08 13:48:10 +0000

    modified:   include.modern.js 	modified:   package.json

--- include.modern.js
+++ include.modern.js
@@ -1,17 +1,22 @@
-let include = (Object.getPrototypeOf(this.constructor.prototype) !== null && Object.getPrototypeOf(this.constructor.prototype).constructor.name === "WorkerGlobalScope") ?
-        importScripts
-    : this.constructor.name === "Object" ?
-        function (...a) {
-            return a.reduceRight((p, c) => Object.assign(p, c.constructor === Object ?
-                Object.keys(c).forEach((k) => c[k] = require(c[k])) || c
-            :
-                {[c.slice(c.length - 3) === ".js" ? c.slice(0, c.length - 3) : c]: require(c)}
-            ), a[a.length - 1].constructor === String ? this : a.pop());
-        }
-    :
-        function (...a) {
-            let s = document.createElement("script");
-            s.setAttribute("src", a.shift());
-            a.length > 0 && s.addEventListener.addEventListener("load", include(...a));
-            document.body.appendChild(s);
-        };
+(() => {
+    let include = (Object.getPrototypeOf(this.constructor.prototype) !== null && Object.getPrototypeOf(this.constructor.prototype).constructor.name === "WorkerGlobalScope") ?
+            importScripts
+        : this.constructor.name === "Object" ?
+            function (...a) {
+                return a.reduceRight((p, c) => Object.assign(p, c.constructor === Object ?
+                    Object.keys(c).forEach((k) => c[k] = require(c[k])) || c
+                :
+                    {[c.slice(c.length - 3) === ".js" ? c.slice(0, c.length - 3) : c]: require(c)}
+                ), a[a.length - 1].constructor === String ? this : a.pop());
+            }
+        :
+            function (...a) {
+                let s = document.createElement("script");
+                s.setAttribute("src", a.shift());
+                a.length > 0 && s.addEventListener.addEventListener("load", include(...a));
+                document.body.appendChild(s);
+            };
+
+    this.constructor.name === "Object" && (module.exports = include);
+    this.constructor.name === "Object" || Object.assign(this, {include});
+})();
