d0ef06d23373fa8e197e290aa6a91c2c8b857142
johnny_shaman <johnny_shaman@live.jp>
2018-04-08 15:29:34 +0000

    modified:   include.modern.js

--- include.modern.js
+++ include.modern.js
@@ -1,5 +1,5 @@
 (() => {
-    let setSrc = (a, b) => a.setAttribute("src", b) || a;
+    let setAttr = (a, b) => Object.keys(b).forEach(k => a.setAttribute(k, b[k])) || a;
     let onLoad = (a, b) => a.addEventListener("load", b, false) || a;
     let FNToMN = (s) => s.slice(s.length - 3) === ".js" ? s.slice(0, s.length - 3) : s;
     let include = (Object.getPrototypeOf(this.constructor.prototype) !== null && Object.getPrototypeOf(this.constructor.prototype).constructor.name === "WorkerGlobalScope") ?
@@ -14,9 +14,13 @@
             }
         :
             function (...a) {
-                let c = a.shift();
-                c !== undefined && this[FNToMN(c)] === undefined ? document.body.appendChild(onLoad(setSrc(
-                    document.createElement("script"), c
+                const c = a.shift();
+                c !== undefined && this[FNToMN(c)] === undefined ? document.body.appendChild(onLoad(setAttr(
+                    document.createElement("script"), {
+                        "async": "",
+                        "defer": "",
+                        "src": c,
+                    }
                 ), () => include(...a))): include(...a);
             };
 
