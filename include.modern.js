3c00dab8f4b0fc24cd273117f207d5dc6f951e17
johnny_shaman <johnny_shaman@live.jp>
2018-04-13 09:19:02 +0000

    modified:   include.modern.js 	modified:   package.json

--- include.modern.js
+++ include.modern.js
@@ -1,20 +1,27 @@
 (() => {
+    Object.defineProperties(Promise.prototype, {
+        defer: {
+            configurable: true,
+            get () {
+                return this.then;
+            }
+        }
+    });
     let setAttr = (a, b) => Object.keys(b).forEach(k => a.setAttribute(k, b[k])) || a;
     let onLoad = (a, b) => a.addEventListener("load", b, false) || a;
     let props = {
-        paths: ["./", "../"],
-
+        paths: ["", "./", "../"],
+        imported: [],
         setPath (...a) {
             include.paths = include.paths.concat(a);
         },
-
         toName (s) {
             s.slice(s.length - 3) === ".js" ?
                 include.paths.map(v => s.slice(0, v.length) === v ? s.slice(v.length, s.length - 3) : undefined).reduce((p, c) => c !== undefined ? c : p, s.slice(0, s.length - 3))
             : s;
         }
     };
-
+    
     let include = (Object.getPrototypeOf(this.constructor.prototype) !== null && Object.getPrototypeOf(this.constructor.prototype).constructor.name === "WorkerGlobalScope") ?
             importScripts
         : this.constructor.name === "Object" ?
@@ -26,17 +33,18 @@
                 ), (a[a.length - 1].constructor === String || Object.keys(a[a.length - 1]).length > 0) ? this : a.pop());
             }, props)
         :
-            Object.assign(function (...a) {
-                const c = a.shift();
-                c !== undefined && this[include.toName(c)] === undefined ? document.body.appendChild(onLoad(setAttr(
-                    document.createElement("script"), {
-                        "async": "",
-                        "defer": "",
-                        "src": c,
-                    }
-                ), () => include(...a))): include(...a);
+            Object.assign(async function (...a) {
+                const src = src.length > 0 && a.shift();
+                src && include.imported.reduceRight((p, c) => c !== src && p, true) && document.body.append(onLoad(setAttr(document.createElement("script"), {
+                    "async": "",
+                    "defer": "",
+                    "src": src
+                }), async function handleLoad (e) {
+                    e.target.removeEventListener("load", handleLoad, false);
+                    include.imported.push(src);
+                    await include(...a);
+                }));
             }, props);
 
-    this.constructor.name === "Object" && (module.exports = include);
-    this.constructor.name === "Object" || Object.assign(this, {include});
+    this.constructor.name === "Object" ? (module.exports = include) : Object.assign(this, include);
 })();
